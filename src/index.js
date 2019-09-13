e6425565979b131effffba2fc167af11f7d672e2
Matheus Allan <matheusrpd@gmail.com>
2019-09-13 17:15:56 -0300

    Adicionado funçao de liberar acesso as imagens estaticamente.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
 mongoose.connect(
   "mongodb+srv://admin:asd147890@cluster0-sh8z3.mongodb.net/test?retryWrites=true&w=majority",
   {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
   }
 );
+
+// Acessa os arquivos estáticos
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "uploads", "resized"))
+);
+
+// Libera acesso para outros clientes
+app.use(cors());
 
 app.use(require("./routes"));
 
